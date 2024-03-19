bc0713dce77554858be61d7f35551d2634b8c472
Fillipe143 <fillipedev.contato@gmail.com>
2024-03-19 08:09:41 -0300

    Agora ele consegue ler dois sensores ao mesmo tempo

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,56 +1,88 @@
-#include <Wire.h>
 #include <Adafruit_PCF8574.h>
-//#include <BluetoothSerial.h>
+#include <Adafruit_VL53L0X.h>
 
-// set the pins to shutdown
-#define xShut1 0
-#define xShut2 1 //não esta no pino certo
-#define xShut3 2
-#define xShut4 3 //não esta no pino certo
-#define xShut5 4 //não esta no pino certo
-#define xShut6 5 //não esta no pino certo
-#define xShut7 6 //não esta no pino certo
-#define xShut8 7 //não esta no pino certo
+#define PCF_ADDR 0x20
 
+#define N_SENSOR 8
+#define LOX1_ADDR 1
+#define LOX2_ADDR 2
+#define LOX3_ADDR 3
+#define LOX4_ADDR 4
+#define LOX5_ADDR 5
+#define LOX6_ADDR 6
+#define LOX7_ADDR 7
 
 Adafruit_PCF8574 pcf;
-//BluetoothSerial SerialBT;
+Adafruit_VL53L0X lox_1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox_2 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox_3 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox_4 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox_5 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox_6 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox_7 = Adafruit_VL53L0X();
 
 void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-  SerialBT.begin("baratinhaESP");
-  setupLidars();
+    Serial.begin(115200);
+    setupSensors();
 }
 
 void loop() {
-  if (SerialBT.available()) {
-    Serial.println("Enviando");
-    Wire.beginTransmission(0x08);
-    while (SerialBT.available()) Wire.write(SerialBT.read());
-    Wire.endTransmission();
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  //Serial.print("Reading a measurement... ");
+  lox_1.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance 1 (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
   }
+  
+  lox_2.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance 2 (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+
+  Serial.println();
+    
+  delay(400);
 }
 
-void setupLidars() {
-  pcf.pinMode(xShut1, OUTPUT);
-  pcf.pinMode(xShut2, OUTPUT);
-  pcf.pinMode(xShut3, OUTPUT);
-  pcf.pinMode(xShut4, OUTPUT);
-  pcf.pinMode(xShut5, OUTPUT);
-  pcf.pinMode(xShut6, OUTPUT);
-  pcf.pinMode(xShut7, OUTPUT);
-  pcf.pinMode(xShut8, OUTPUT);
 
-  Serial.println("INICIANDO");
-  setID();
-  for(int i = 0; i < numSenDist; i++) {
-    vl[i].init();
-    vl[i].setTimeout(500);
-    vl[i].startContinuous();
-    vl[i].setMeasurementTimingBudget(modoSensor);
-  }
-  Serial.println("Terminada configuração dos LIDARs");
+void setupSensors() {
+    if (!pcf.begin(PCF_ADDR, &Wire)) {
+        Serial.println("Couldn't find PCF");
+        while (1);
+    }
+    
+    Serial.println("Desligando sensores...");
+    for (uint8_t sensor = 1; sensor <= N_SENSOR; sensor++) {
+        pcf.digitalWrite(sensor, LOW);
+    }
 
+
+    Serial.println("Configurando...");
+    
+    Serial.println("Sensor " + String(LOX1_ADDR));
+    pcf.digitalWrite(LOX1_ADDR, HIGH); 
+    if (!lox_1.begin()) {
+      Serial.println(("Failed to boot VL53L0X"));
+      while(1);
+    }
+    lox_1.setAddress(0x29 + LOX1_ADDR);
+
+
+    Serial.println("Sensor " + String(LOX2_ADDR));
+    pcf.digitalWrite(LOX2_ADDR, HIGH); 
+    if (!lox_2.begin()) {
+      Serial.println(("Failed to boot VL53L0X"));
+      while(1);
+    }
+    lox_2.setAddress(0x29 + LOX2_ADDR);
+
+    Serial.println("Terminou a configuração");
 }
-
